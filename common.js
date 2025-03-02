f46e4b3a5ea064748f3253d604ca08ba09d5b0fd
sky25680732 <165028173+sky25680732@users.noreply.github.com>
2025-03-02 15:48:17 +0800

    Add files via upload

---
+++ common.js
@@ -0,0 +1,11 @@
+$('.owl-carousel').owlCarousel({
+    items: 4,
+    stagePadding: 200,
+    loop: true,
+    margin: 40,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    smartSpeed: 2000,
+    slideTransition:'linear',
+})
